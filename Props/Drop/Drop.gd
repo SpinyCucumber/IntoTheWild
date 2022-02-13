870d69fe250e7e9098209502baa33551390d45f1
ethilty <spinycucumber@gmail.com>
2022-02-12 22:21:09 -0600

    Trees spawn sticks

--- Props/Drop/Drop.gd
+++ Props/Drop/Drop.gd
@@ -1,5 +1,8 @@
 extends "res://Props/Interactable/Interactable.gd"
 
+signal picked_up
+
 func pickup():
 	# TODO
+	emit_signal("picked_up")
 	queue_free()
---
+++ Props/Tree/Tree.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+const Stick = preload("res://Props/Tree/Stick.tscn")
+
+func spawn_stick():
+	var stick = Stick.instance();
+	Globals.area_system.current_area.add_child(stick);
+	stick.global_position = global_position;
+	stick.connect("picked_up", self, "_on_stick_picked_up")
+
+func _on_stick_picked_up():
+	$StickTimer.start();
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
